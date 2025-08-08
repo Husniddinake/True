5e93452cd1562c91232ba7262f9a9a5d70feb10d
Abu <abu@gmail.com>
2025-08-08 18:13:47 +0500

    wwdwd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Part from './components/Part2.jsx'
-
+import Abushka from './components/Abu.jsx'
 const App = () => {
   return (
+    <div>
     <Part/>
+    <Abushka/>
+    </div>
   )
 }
 
